a078ecd40b002e08b576bc7ab525e73d8ed1120e
Unknown <axel.uran@epfl.ch>
2017-11-13 14:16:14 +0100

    dictionary added

--- Preprocessing/reversegeo.py
+++ Preprocessing/reversegeo.py
@@ -1,8 +1,77 @@
 import reverse_geocoder as rg
 import csv
 import multiprocessing as mp
+import multiprocessing.pool
 import glob
 import re
+
+mx_ca_us_state_abbrev = {
+    'Alabama': '1',
+    'Alaska': '2',
+    'Arizona': '3',
+    'Arkansas': '4',
+    'California': '5',
+    'Colorado': '6',
+    'Connecticut': '7',
+    'Delaware': '8',
+    'Florida': '9',
+    'Georgia': '10',
+    'Hawaii': '11',
+    'Idaho': '12',
+    'Illinois': '13',
+    'Indiana': '14',
+    'Iowa': '15',
+    'Kansas': '16',
+    'Kentucky': '17',
+    'Louisiana': '18',
+    'Maine': '19',
+    'Maryland': '20',
+    'Massachusetts': '21',
+    'Michigan': '22',
+    'Minnesota': '23',
+    'Mississippi': '24',
+    'Missouri': '25',
+    'Montana': '26',
+    'Nebraska': '27',
+    'Nevada': '28',
+    'New Hampshire': '29',
+    'New Jersey': '30',
+    'New Mexico': '31',
+    'New York': '32',
+    'North Carolina': '33',
+    'North Dakota': '34',
+    'Ohio': '35',
+    'Oklahoma': '36',
+    'Oregon': '37',
+    'Pennsylvania': '38',
+    'Rhode Island': '39',
+    'South Carolina': '40',
+    'South Dakota': '41',
+    'Tennessee': '42',
+    'Texas': '43',
+    'Utah': '44',
+    'Vermont': '45',
+    'Virginia': '46',
+    'Washington': '47',
+    'West Virginia': '48',
+    'Wisconsin': '49',
+    'Wyoming': '50',
+    'Ontario': '51',
+    'Quebec': '52',
+    'Nova Scotia': '53',
+    'New Brunswick': '54',
+    'Manitoba': '55',
+    'British Columbia': '56',
+    'Prince Edward': '57',
+    'Saskatchewan': '58',
+    'Alberta': '59',
+    'Newfoundland and Labrador': '60',
+    'Washington, D.C.': '61',
+    'Chihuahua': '62',
+    'Baja California': '63',
+    'Freeport': '64',
+    'Nuevo Leon': '65',
+}
 
 # coordinates = (30.5029812,-84.2449241)
 #
@@ -10,7 +79,7 @@ import re
 #
 # print(results)
 
-NUM_OF_PROCESSES = 1
+NUM_OF_PROCESSES = 4
 
 def ensure_output_paths_exist():
     """Maybe we will not use this since we will be editing the files directly"""
@@ -33,7 +102,7 @@ def run_all(path):
     # ensure_output_paths_exist()
 
     # If NUM_OF_PROCESSES is False, use mp.cpu_count
-    pool = mp.Pool(NUM_OF_PROCESSES or mp.cpu_count())
+    pool = multiprocessing.pool.ThreadPool(NUM_OF_PROCESSES or mp.cpu_count())
 
     pool.map(gzworker, file_paths, chunksize=1)
 
@@ -100,6 +169,12 @@ def gzworker(fullpath):
             coordinates = (lat,lon)
             results = rg.search(coordinates) # default mode = 2
             print(results)
+            state_num = mx_ca_us_state_abbrev.get(results[0].get('admin1'))
+            print(state_num)
+
+            # state_num = us_state_abbrev.results['admin1']
+            # print(state_num)
+# [('lat', '29.23329'), ('lon', '-98.79641'), ('name', 'Lytle'), ('admin1', 'Texas'), ('admin2', 'Atascosa County'), ('cc', 'US')]
 
     # except:
     #     print("Error in {}".format(fullpath))
--- Preprocessing/test_geo.py
+++ Preprocessing/test_geo.py
@@ -1,4 +1,4 @@
 from reversegeo import run_all, gzworker
 
-# run_all("Dump")
-gzworker('Dump/tweets.txt.2011-04-27_08.csv')
+run_all("Dump")
+# gzworker('Dump/tweets.txt.2011-04-27_08.csv')
