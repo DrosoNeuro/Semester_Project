3ea7c4e92b6d64aced1568ab9957798c223226c0
Unknown <axel.uran@epfl.ch>
2017-11-14 12:33:52 +0100

    Filter will also do the preprocessing

--- Preprocessing/filter.py
+++ Preprocessing/filter.py
@@ -1,6 +1,7 @@
 from collections import namedtuple
 from itertools import repeat
 
+import reverse_geocoder as rg
 import gzip
 import os
 import io
@@ -9,10 +10,79 @@ import re
 import string
 import sys
 import multiprocessing as mp
+import multiprocessing.pool
 import glob
 import uuid
 import configparser
 import csv
+
+mx_ca_us_state_abbrev = {
+    'Alabama': '1',
+    'Alaska': '2',
+    'Arizona': '3',
+    'Arkansas': '4',
+    'California': '5',
+    'Colorado': '6',
+    'Connecticut': '7',
+    'Delaware': '8',
+    'Florida': '9',
+    'Georgia': '10',
+    'Hawaii': '11',
+    'Idaho': '12',
+    'Illinois': '13',
+    'Indiana': '14',
+    'Iowa': '15',
+    'Kansas': '16',
+    'Kentucky': '17',
+    'Louisiana': '18',
+    'Maine': '19',
+    'Maryland': '20',
+    'Massachusetts': '21',
+    'Michigan': '22',
+    'Minnesota': '23',
+    'Mississippi': '24',
+    'Missouri': '25',
+    'Montana': '26',
+    'Nebraska': '27',
+    'Nevada': '28',
+    'New Hampshire': '29',
+    'New Jersey': '30',
+    'New Mexico': '31',
+    'New York': '32',
+    'North Carolina': '33',
+    'North Dakota': '34',
+    'Ohio': '35',
+    'Oklahoma': '36',
+    'Oregon': '37',
+    'Pennsylvania': '38',
+    'Rhode Island': '39',
+    'South Carolina': '40',
+    'South Dakota': '41',
+    'Tennessee': '42',
+    'Texas': '43',
+    'Utah': '44',
+    'Vermont': '45',
+    'Virginia': '46',
+    'Washington': '47',
+    'West Virginia': '48',
+    'Wisconsin': '49',
+    'Wyoming': '50',
+    'Ontario': '51',
+    'Quebec': '52',
+    'Nova Scotia': '53',
+    'New Brunswick': '54',
+    'Manitoba': '55',
+    'British Columbia': '56',
+    'Prince Edward': '57',
+    'Saskatchewan': '58',
+    'Alberta': '59',
+    'Newfoundland and Labrador': '60',
+    'Washington, D.C.': '61',
+    'Chihuahua': '62',
+    'Baja California': '63',
+    'Freeport': '64',
+    'Nuevo Leon': '65',
+}
 
 ##############################################################################
 ################### Configurable Params ######################################
@@ -47,7 +117,7 @@ def run_all(path):
     ensure_output_paths_exist()
 
     # If NUM_OF_PROCESSES is False, use mp.cpu_count
-    pool = mp.Pool(NUM_OF_PROCESSES or mp.cpu_count())
+    pool = multiprocessing.pool.ThreadPool(NUM_OF_PROCESSES or mp.cpu_count())
 
     pool.starmap(gzworker, zip(file_paths, repeat(STRIP)), chunksize=1)
 
@@ -116,6 +186,13 @@ def tweet_select(tweet_obj):
         tweet_coordinates = tweet_obj["place"]['bounding_box']['coordinates']
         tweet_type = "place"
 
+    geoloc = tweet_coordinates.split(',')
+    lon = geoloc[0].replace('[', '')
+    lat = geoloc[1].replace(']', '').replace(' ', '')
+
+    coordinates = (lat,lon)
+    results = rg.search(coordinates) # default mode = 2
+    state_num = mx_ca_us_state_abbrev.get(results[0].get('admin1'))
 
     big_block = []
     big_block.append(tweet_id)
@@ -123,6 +200,7 @@ def tweet_select(tweet_obj):
     big_block.append(tweet_text)
     big_block.append(tweet_coordinates)
     big_block.append(tweet_type)
+    big_block.append(state_num)
     return big_block
 
 
